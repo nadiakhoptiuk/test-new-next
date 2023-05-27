178ec627ba6b802b086cb496a8b12617fae6f900
Nadiia Khoptiuk <holineinadia@gmail.com>
2023-05-27 12:00:56 +0300

    add start files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+};
